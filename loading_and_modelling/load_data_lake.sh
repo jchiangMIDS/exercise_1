0935a16e3de7991c5bbf60b0a2fd353493e77642
jchiangMIDS <jchiang@ischool.berkeley.edu>
2016-06-13 17:47:23 -0400

    Update load_data_lake.sh
    code to put raw data into data lake

--- loading_and_modelling/load_data_lake.sh
+++ loading_and_modelling/load_data_lake.sh
@@ -9,3 +9,16 @@ tail -n +2 ~/Documents/W205/Measure_Dates.csv > ~/Documents/W205/Measures.csv
 tail -n +2 ~/Documents/W205/hvbp_hcahps_05_28_2015.csv > ~/Documents/W205/surverys_responses.csv
 
 # Load the datasets into HDFS
+
+wget https://github.com/jchiangMIDS/exercise_1/blob/master/loading_and_modelling/hospitals.csv
+wget https://github.com/jchiangMIDS/exercise_1/blob/master/loading_and_modelling/effective_care.csv.zip
+wget https://github.com/jchiangMIDS/exercise_1/blob/master/loading_and_modelling/readmissions.csv
+wget https://github.com/jchiangMIDS/exercise_1/blob/master/loading_and_modelling/Measures.csv
+wget https://github.com/jchiangMIDS/exercise_1/blob/master/loading_and_modelling/surverys_responses.csv
+
+hdfs dfs -put hospitals.csv /user/w205/hospital_compare
+hdfs dfs -put effective_care.csv.zip /user/w205/hospital_compare
+hdfs dfs -put readmissions.csv /user/w205/hospital_compare
+hdfs dfs -put Measures.csv /user/w205/hospital_compare
+hdfs dfs -put surveys_responses.csv /user/w205/hospital_compare
+
