c224e39e67eb8589076e3b205aa6b586f6dc6f97
jchiangMIDS <jchiang@ischool.berkeley.edu>
2016-06-17 00:25:04 +0200

    Update hive_base_ddl.sql

--- loading_and_modelling/hive_base_ddl.sql
+++ loading_and_modelling/hive_base_ddl.sql
@@ -1,13 +1,3 @@
-DROP TABLE <table_name>;
-CREATE EXTERNAL TABLE <table_name> (<col1_name>, <col1_type>, …)
-ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
-WITH SERDEPROPERTIES (
- "separatorChar" = ",",
- "quoteChar" = '"',
-  "escapeChar" = '\\'
-)
-STORED AS TEXTFILE
-LOCATION ‘/path/in/hdfs’;
 
 DROP TABLE effective_care;
 CREATE EXTERNAL TABLE effective_care (Provider_Number STRING, Hospital_Name STRING,
@@ -40,7 +30,7 @@ DROP TABLE responses;
 CREATE EXTERNAL TABLE responses (Provider_ID STRING, Hospital_name STRING, 
 Address STRING, City STRING, State STRING, ZIP_Code STRING, County_name STRING, 
 Phone_number STRING, Condition STRING, Measure_ID STRING, Measure_name STRING, 
-Score INT, Sample INT, Footnote STRING, Measure_Start_Date DATE, Measure_End_Date DATE)
+Score BIGINT, Sample BIGINT, Footnote STRING, Measure_Start_Date DATE, Measure_End_Date DATE)
 ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
 WITH SERDEPROPERTIES (
  "separatorChar" = ",",
