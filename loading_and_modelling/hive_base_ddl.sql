f1243690f8e8b40abc33ea54aba00b1de5e95d5b
jchiangMIDS <jchiang@ischool.berkeley.edu>
2016-06-16 10:31:37 +0200

    Update hive_base_ddl.sql

--- loading_and_modelling/hive_base_ddl.sql
+++ loading_and_modelling/hive_base_ddl.sql
@@ -1,1 +1,24 @@
+DROP TABLE <table_name>;
+CREATE EXTERNAL TABLE <table_name> (<col1_name>, <col1_type>, …)
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
+WITH SERDEPROPERTIES (
+ "separatorChar" = ",",
+ "quoteChar" = '"',
+  "escapeChar" = '\\'
+)
+STORED AS TEXTFILE
+LOCATION ‘/path/in/hdfs’;
 
+DROP TABLE effective_care;
+CREATE EXTERNAL TABLE effective_care (Provider_ID STRING, Hospital_name STRING, 
+Address STRING, City STRING, State STRING, ZIP_Code STRING, County_name STRING, 
+Phone_number STRING, Condition STRING, Measure_ID STRING, Measure_name STRING, 
+Score INT, Sample INT, Footnote STRING, Measure_Start_Date DATE, Measure_End_Date DATE)
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
+WITH SERDEPROPERTIES (
+ "separatorChar" = ",",
+ "quoteChar" = '"',
+  "escapeChar" = '\\'
+)
+STORED AS TEXTFILE
+LOCATION ‘/path/in/hdfs’;
