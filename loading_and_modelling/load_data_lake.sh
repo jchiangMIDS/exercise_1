b7671abdc9a0ff5dda96528514a88e20fa828f60
jchiangMIDS <jchiang@ischool.berkeley.edu>
2016-06-21 16:49:21 -0700

    Update load_data_lake.sh
    Added individual folders for each dataset

--- loading_and_modelling/load_data_lake.sh
+++ loading_and_modelling/load_data_lake.sh
@@ -1,15 +1,13 @@
-# strip header lines from needed files
-tail -n +2 ~/Documents/W205/Hospital_General_Information.csv > ~/Documents/W205/hospitals.csv
+# strip header lines from needed files in local directory
 tail -n +2 ~/Documents/W205/Timely_and_Effective_Care_-_Hospital.csv > ~/Documents/W205/effective_care.csv
-tail -n +2 ~/Documents/W205/Readmissions_and_Deaths_-_Hospital.csv > ~/Documents/W205/readmissions.csv
-tail -n +2 ~/Documents/W205/Measure_Dates.csv > ~/Documents/W205/Measures.csv
 tail -n +2 ~/Documents/W205/hvbp_hcahps_05_28_2015.csv > ~/Documents/W205/surverys_responses.csv
 
 #Start HDFS, Hadoop Yarn and Hive:
 /root/start-hadoop.sh
 
-# Make a new folder in HDFS for this lab: 
-hdfs dfs -mkdir /user/w205/hospital_compare
+# Make a new folders in HDFS for this lab: 
+hdfs dfs -mkdir /user/w205/hospital_compare/effective_care
+hdfs dfs -mkdir /user/w205/hospital_compare/surveys_responses
 
 # Upload files into w205 instance
 git clone https://github.com/jchiangMIDS/exercise_1
@@ -17,9 +15,7 @@ cd exercise_1/loading_and_modelling
 unzip effective_care.zip
 
 # Load the datasets into HDFS
-hdfs dfs -put hospitals.csv /user/w205/hospital_compare
-hdfs dfs -put effective_care.csv /user/w205/hospital_compare
-hdfs dfs -put readmissions.csv /user/w205/hospital_compare
-hdfs dfs -put Measures.csv /user/w205/hospital_compare
-hdfs dfs -put surveys_responses.csv /user/w205/hospital_compare
+hdfs dfs -put effective_care.csv /user/w205/hospital_compare/effective_care
+hdfs dfs -put surveys_responses.csv /user/w205/hospital_compare/surveys_responses
+
 
