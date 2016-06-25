9222131c7413dbb8e254e349070f8ce4a8f1747b
jchiangMIDS <jchiang@ischool.berkeley.edu>
2016-06-25 14:36:55 -0700

    Create variety_score.sql

---
+++ transforming/variety_score.sql
@@ -0,0 +1,7 @@
+#create variety score on a scale of 0.1 - 100.1
+CREATE TABLE variety_score
+   ROW FORMAT SERDE "org.apache.hadoop.hive.serde2.columnar.ColumnarSerDe"
+   STORED AS RCFile
+   AS
+SELECT Provider_ID, Measure_Count, Measure_Count/51*100+0.1
+FROM variety;
