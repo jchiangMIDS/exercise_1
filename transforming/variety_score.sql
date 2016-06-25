39750f507c6fb1d779a6840bafaac997cedd0d79
jchiangMIDS <jchiang@ischool.berkeley.edu>
2016-06-25 15:05:22 -0700

    Update variety_score.sql

--- transforming/variety_score.sql
+++ transforming/variety_score.sql
@@ -3,5 +3,5 @@ CREATE TABLE variety_score
    ROW FORMAT SERDE "org.apache.hadoop.hive.serde2.columnar.ColumnarSerDe"
    STORED AS RCFile
    AS
-SELECT Provider_ID, Measure_Count, Measure_Count/51*100+0.1
+SELECT Provider_ID, Measure_Count, Measure_Count/51*100+0.1 AS Variety_Score
 FROM variety;
