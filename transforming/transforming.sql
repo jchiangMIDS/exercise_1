edb012ec34151f8e1bce89fd35c52dfc58f5833c
jchiangMIDS <jchiang@ischool.berkeley.edu>
2016-06-24 19:11:44 -0700

    Update transforming.sql

--- transforming/transforming.sql
+++ transforming/transforming.sql
@@ -1,8 +1,8 @@
 
 #Createatable for slimmed down maintable now called procedures
 CREATE TABLE procedures
-   ROW FORMAT SERDE "org.apache.hadoop.hive.serde2.OpenCSVSerde"
-   STORED AS TEXTFILE
+   ROW FORMAT SERDE "org.apache.hadoop.hive.serde2.columnar.ColumnarSerDe"
+   STORED AS RCFile
    AS
 SELECT Provider_ID, Measure_ID, Score, Sample, State, Hospital_Name 
 FROM effectivecare_schema;
@@ -10,8 +10,8 @@ FROM effectivecare_schema;
 
 #Createatable for slimmed down surveytable called surveys
 CREATE TABLE surveys
-   ROW FORMAT SERDE "org.apache.hadoop.hive.serde2.OpenCSVSerde"
-   STORED AS TEXTFILE
+   ROW FORMAT SERDE "org.apache.hadoop.hive.serde2.columnar.ColumnarSerDe"
+   STORED AS RCFile
    AS
 SELECT Provider_Number, HCAHPS_Base_Score, HCAHPS_Consistency_Score
 FROM surveys_responses_schema;
@@ -19,18 +19,18 @@ FROM surveys_responses_schema;
 
 #Transform data to have Min, Max, and Range for procedure scores as dilneated by Measure_ID
 CREATE TABLE scoremetrics
-   ROW FORMAT SERDE "org.apache.hadoop.hive.serde2.OpenCSVSerde"
-   STORED AS TEXTFILE
+   ROW FORMAT SERDE "org.apache.hadoop.hive.serde2.columnar.ColumnarSerDe"
+   STORED AS RCFile
    AS
-SELECT Measure_ID, MAX(Score) AS MaxScore, MIN(Score) AS MinScore, MAX(Score)-MIN(Score) As Range 
-FROM effectivecare_schema 
+SELECT Measure_ID, MIN(Score*100/100) AS MinScore, MAX(Score*100/100) AS MaxScore, MAX(Score*100/100)-MIN(Score*100/100) As Range 
+FROM effectivecare_schema
 WHERE Score BETWEEN 0 AND 5000 GROUP BY Measure_ID;
 
 
 #Merge new scoremetric values with procedures
 CREATE TABLE procedures_m
-   ROW FORMAT SERDE "org.apache.hadoop.hive.serde2.OpenCSVSerde"
-   STORED AS TEXTFILE
+   ROW FORMAT SERDE "org.apache.hadoop.hive.serde2.columnar.ColumnarSerDe"
+   STORED AS RCFile
    AS
 SELECT procedures.Provider_ID, procedures.Measure_ID, procedures.Score, 
 scoremetrics.MinScore, scoremetrics.MaxScore, scoremetrics.Range, procedures.Hospital_Name, procedures.State 
@@ -40,20 +40,27 @@ LEFT JOIN scoremetrics ON procedures.Measure_ID = scoremetrics.Measure_ID;
 
 #calculate procedure_score that puts all scores on same 0to100 scale
 CREATE TABLE procedure_calc
-   ROW FORMAT SERDE "org.apache.hadoop.hive.serde2.OpenCSVSerde"
-   STORED AS TEXTFILE
+   ROW FORMAT SERDE "org.apache.hadoop.hive.serde2.columnar.ColumnarSerDe"
+   STORED AS RCFile
    AS
-SELECT Provider_ID, Measure_ID, (Score-MIN(Score))/Range*100 AS Procedure_Score, Hospital_Name, State
-FROM procedures_m;
+SELECT Provider_ID, Measure_ID, (Score-MinScore)/Range*100 AS Procedure_Score, Hospital_Name, State
+FROM procedures_m GROUP BY Provider_ID, Measure_ID;
 
 
 #Merge new scoremetric values with procedures
 CREATE TABLE hospitals
-   ROW FORMAT SERDE "org.apache.hadoop.hive.serde2.OpenCSVSerde"
-   STORED AS TEXTFILE
+   ROW FORMAT SERDE "org.apache.hadoop.hive.serde2.columnar.ColumnarSerDe"
+   STORED AS RCFile
    AS
 SELECT procedure_calc.Provider_ID, procedure_calc.Measure_ID, procedure_calc.Procedure_Score, 
 surveys.HCAHPS_Base_Score, surveys.HCAHPS_Consistency_Score, procedure_calc.State, procedure_calc.Hospital_Name
 FROM surveys
 LEFT JOIN procedure_calc ON procedure_calc.Provider_ID = surveys.Provider_Number;
+
+
+
+
+
+
+
 
