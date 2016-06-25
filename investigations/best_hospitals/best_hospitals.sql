82207bfd5d94697382ae66b1d379fffdc243393b
jchiangMIDS <jchiang@ischool.berkeley.edu>
2016-06-25 15:41:52 -0700

    Update best_hospitals.sql

--- investigations/best_hospitals/best_hospitals.sql
+++ investigations/best_hospitals/best_hospitals.sql
@@ -1,7 +1,14 @@
 #Rank hospitals by best average procedure_scores across all their procedures
+CREATE TABLE best_provider
+   ROW FORMAT SERDE "org.apache.hadoop.hive.serde2.columnar.ColumnarSerDe"
+   STORED AS RCFile
+   AS
+SELECT Hospital_Name, AVG(Procedure_score) AS procedure, AVG(Variety_score) AS variety
+FROM hospitals_scores 
+GROUP BY Hospital_Name;
 
-SELECT Hospital_Name, AVG(Procedure_score) AS score 
-FROM hospitals 
-GROUP BY Hospital_Name 
-ORDER BY score DESC
-LIMIT 100;
+
+  SELECT Hospital_Name, procedure*variety as scores, procedure, variety
+  FROM best_provider
+  ORDER BY scores DESC
+  LIMIT 20;
