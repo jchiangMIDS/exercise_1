8b78389dce239ecb380bdefab68bf1d7ec69339d
jchiangMIDS <jchiang@ischool.berkeley.edu>
2016-06-25 13:40:39 -0700

    Update best_hospitals.sql

--- investigations/best_hospitals/best_hospitals.sql
+++ investigations/best_hospitals/best_hospitals.sql
@@ -1,1 +1,7 @@
-#Get best hospitals by score in oder
+#Rank hospitals by best average procedure_scores across all their procedures
+
+SELECT Hospital_Name, AVG(Procedure_score) AS score 
+FROM hospitals 
+GROUP BY Hospital_Name 
+ORDER BY score DESC
+LIMIT 100;
