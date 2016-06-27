0fe6cbf959c931776da7a578fe53ebe8fe547a71
jchiangMIDS <jchiang@ischool.berkeley.edu>
2016-06-26 17:22:59 -0700

    Update procedures_m.sql

--- transforming/procedures_m.sql
+++ transforming/procedures_m.sql
@@ -1,4 +1,3 @@
-#Transform data to have Min, Max, and Range for procedure scores as dilneated by Measure_ID
 CREATE TABLE scoremetrics
    ROW FORMAT SERDE "org.apache.hadoop.hive.serde2.columnar.ColumnarSerDe"
    STORED AS RCFile
@@ -8,7 +7,7 @@ FROM effectivecare_schema
 WHERE Score BETWEEN 0 AND 5000 GROUP BY Measure_ID;
 
 
-#Merge new scoremetric values with procedures
+
 CREATE TABLE procedures_m
    ROW FORMAT SERDE "org.apache.hadoop.hive.serde2.columnar.ColumnarSerDe"
    STORED AS RCFile
