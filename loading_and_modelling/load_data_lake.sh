c14f36059e88a3c8f0f3190c384571cbbc861e99
jchiangMIDS <jchiang@ischool.berkeley.edu>
2016-06-26 17:57:47 -0700

    Update load_data_lake.sh

--- loading_and_modelling/load_data_lake.sh
+++ loading_and_modelling/load_data_lake.sh
@@ -1,9 +1,9 @@
 # strip header lines from needed files in local directory
-tail -n +2 ~/Documents/W205/Timely_and_Effective_Care_-_Hospital.csv > ~/Documents/W205/effective_care.csv
-tail -n +2 ~/Documents/W205/hvbp_hcahps_05_28_2015.csv > ~/Documents/W205/surverys_responses.csv
+#tail -n +2 ~/Documents/W205/Timely_and_Effective_Care_-_Hospital.csv > ~/Documents/W205/effective_care.csv
+#tail -n +2 ~/Documents/W205/hvbp_hcahps_05_28_2015.csv > ~/Documents/W205/surverys_responses.csv
 
 #Start HDFS, Hadoop Yarn and Hive:
-/root/start-hadoop.sh
+#/root/start-hadoop.sh
 
 # Make a new folders in HDFS for this lab: 
 hdfs dfs -mkdir /user/w205/hospital_compare
